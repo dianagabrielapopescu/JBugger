51d1fcad358c600252d9931452c872721387838e
Diana Popescu <diana.popescu000@gmail.com>
2019-08-14 15:45:30 +0300

    refactor module2

--- jbugs-web/src/app/app.module.ts
+++ jbugs-web/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {UserModule} from './user/user.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     UserModule,
